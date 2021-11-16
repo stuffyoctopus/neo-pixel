7567b1d4b60fb34ee31e6d67c47423f7833896f0
gkallberg <>
2021-11-16 12:36:55 -0500

    Update neopixel.ino

--- neopixel/neopixel.ino
+++ neopixel/neopixel.ino
@@ -1,11 +1,13 @@
 // NeoPixel Ring simple sketch (c) 2013 Shae Erisson
 // Released under the GPLv3 license to match the rest of the
 // Adafruit NeoPixel library
-
+#include <Wire.h>
+#include "Adafruit_MCP9808.h"
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
+Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 
 // Which pin on the Arduino is connected to the NeoPixels?
 #define PIN        13 // On Trinket or Gemma, suggest changing this to 1
@@ -22,6 +24,14 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 #define DELAYVAL 50 // Time (in milliseconds) to pause between pixels
 
 void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  if (!tempsensor.begin(0x18)) {
+    Serial.println("Couldn't find MCP9808! Check your connections and verify the address is correct.");
+    while (1);
+  }
+  Serial.println("Found MCP9808!");
+  tempsensor.setResolution(3);
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
   // Any other board, you can remove this part (but no harm leaving it):
 #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -34,17 +44,28 @@ void setup() {
 
 void loop() {
   pixels.clear(); // Set all pixel colors to 'off'
+  float c = tempsensor.readTempC();
+  float f = tempsensor.readTempF();
+  Serial.print("Temp: ");
+
+  Serial.print(f, 4); Serial.println("*F.");
+
+  delay(200);
 
   // The first NeoPixel in a strand is #0, second is 1, all the way up
   // to the count of pixels minus one.
-  for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
+  if (f > 82) { // For each pixel...
 
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    pixels.setPixelColor(0, pixels.Color(0, 150, 0));
 
     pixels.show();   // Send the updated pixel colors to the hardware.
 
     delay(DELAYVAL); // Pause before next pass through loop
+  } else {
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
   }
 }
+ 
