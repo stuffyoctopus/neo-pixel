a4ca76774e4eac20ab19addbddbcad60a7384bf0
gkallberg <>
2021-11-16 11:35:49 -0500

    Update neopixel.ino

--- neopixel/neopixel.ino
+++ neopixel/neopixel.ino
@@ -11,7 +11,7 @@
 #define PIN        13 // On Trinket or Gemma, suggest changing this to 1
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 16 // Popular NeoPixel ring size
+#define NUMPIXELS 8 // Popular NeoPixel ring size
 
 // When setting up the NeoPixel library, we tell it how many pixels,
 // and which pin to use to send signals. Note that for older NeoPixel
